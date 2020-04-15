f858e913fb455f80f967a2091d496fcb887a433e
MichaelBoRoHoV <mikelb138@gmail.com>
2020-04-15 17:44:04 +0300

    add props and map to cards.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import NavigationBar from "./NavigationBar";
 import PlayerCard from "./PlayerCard";
+import players from "../players.js"
 
 function App(){
   return (
     <div>
       <NavigationBar />
       <div className="container-cards">
-        <PlayerCard />
+        {players.map(player => (
+          <PlayerCard 
+            key = {player.id}
+            teamName = {player.team}
+            jersey = {player.jersey}
+            fName = {player.firstName}
+            lName = {player.lastName}
+            img = {player.imgURL} alt = "player image"  
+            ppg = {player.stats.ppg}
+            rpg = {player.stats.rpg}
+            apg = {player.stats.apg}
+          />
+        ))};
       </div>
     </div>
   )
